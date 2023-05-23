c6f1cf45eac2bd6ee54866558bf4c9c4acf7a138
Sanith Garipally <sanith.garipally.19.1@mountblue.tech>
2023-05-23 12:55:57 +0530

    feat: Add Loader Component

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Popconfirm } from 'antd';
+import { Popconfirm, Spin } from 'antd';
 import Header from '../Header';
 import UserList from '../UserList';
 import UserPagination from '../UserPagination';
@@ -9,6 +9,7 @@ const apiUrl =
   'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json';
 const Dashboard = () => {
   const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
   const [selectedUsers, setSelectedUsers] = useState([]);
   const [searchText, setSearchText] = useState('');
   const [isSelectAllActive, setSelectAllActive] = useState(false);
@@ -21,6 +22,7 @@ const Dashboard = () => {
         const response = await fetch(apiUrl);
         const data = await response.json();
         setUsers(data);
+        setLoading(false);
       } catch (error) {
         console.error(error.message);
       }
@@ -109,55 +111,61 @@ const Dashboard = () => {
     <>
       <Header />
       <div className='dashboard-container'>
-        <div className='dsh-content-container'>
-          <input
-            className='input-search'
-            value={searchText}
-            onChange={handleFilterUsers}
-            type='search'
-            placeholder='Search by name, email or role'
-          />
-          <ul className='user-list-container'>
-            <li className='list-header'>
-              <input
-                className='checkbox-input'
-                checked={isSelectAllActive}
-                onChange={handleSelectAll}
-                type='checkbox'
-              />
-              <h3 className='header-table-name'>Name</h3>
-              <h3 className='header-table-email'>Email</h3>
-              <h3 className='header-table-role'>Role</h3>
-              <h3 className='header-table-actions'>Actions</h3>
-            </li>
-            {paginatedData.map((user) => (
-              <UserList
-                handleUserSelects={handleUserSelects}
-                selectedUsers={selectedUsers}
-                key={user.id}
-                userItem={user}
-                deleteUser={deleteUser}
-                editUser={editUser}
-              />
-            ))}
-          </ul>
-          <div className='pagination-container'>
-            <Popconfirm
-              title='Are you sure you want to delete selected users?'
-              onConfirm={deleteSelected}
-              okText='Yes'
-              cancelText='No'
-            >
-              <button className='delete-all-btn'>Delete Selected</button>
-            </Popconfirm>
-            <UserPagination
-              onPageChange={handlePagination}
-              currentPage={page}
-              pageSize={usersPerPage}
-              totalUsers={users.length}
-            />
+        {isLoading ? (
+          <div className='loader-container'>
+            <Spin tip='loading...' size='large'/>
           </div>
-        </div>
+        ) : (
+          <div className='dsh-content-container'>
+            <input
+              className='input-search'
+              value={searchText}
+              onChange={handleFilterUsers}
+              type='search'
+              placeholder='Search by name, email or role'
+            />
+            <ul className='user-list-container'>
+              <li className='list-header'>
+                <input
+                  className='checkbox-input'
+                  checked={isSelectAllActive}
+                  onChange={handleSelectAll}
+                  type='checkbox'
+                />
+                <h3 className='header-table-name'>Name</h3>
+                <h3 className='header-table-email'>Email</h3>
+                <h3 className='header-table-role'>Role</h3>
+                <h3 className='header-table-actions'>Actions</h3>
+              </li>
+              {paginatedData.map((user) => (
+                <UserList
+                  handleUserSelects={handleUserSelects}
+                  selectedUsers={selectedUsers}
+                  key={user.id}
+                  userItem={user}
+                  deleteUser={deleteUser}
+                  editUser={editUser}
+                />
+              ))}
+            </ul>
+            <div className='pagination-container'>
+              <Popconfirm
+                title='Are you sure you want to delete selected users?'
+                onConfirm={deleteSelected}
+                okText='Yes'
+                cancelText='No'
+              >
+                <button className='delete-all-btn'>Delete Selected</button>
+              </Popconfirm>
+              <UserPagination
+                onPageChange={handlePagination}
+                currentPage={page}
+                pageSize={usersPerPage}
+                totalUsers={users.length}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
